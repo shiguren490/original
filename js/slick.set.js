c069934841e57df424df5d0ab21bfa777a1138dd
shiguren490 <60349708+shiguren490@users.noreply.github.com>
2020-03-10 22:09:28 +0900

    Update slick.set.js

--- js/slick.set.js
+++ js/slick.set.js
@@ -3,8 +3,10 @@ jQuery(function ($) {
         $('.slider').slick({
             dots: true,
             infinite: true,
+            rows: 2,
+            slidesPerRow: 3,
             slidesToShow: 3,
-            slidesToScroll: 2
+            slidesToScroll: 2,
         });
     });
 });
