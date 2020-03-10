1a2a420603429a3cf86430bb8cc56244e9f3e759
shiguren490 <60349708+shiguren490@users.noreply.github.com>
2020-03-10 21:12:17 +0900

    Add files via upload

---
+++ js/slick.set.js
@@ -0,0 +1,10 @@
+jQuery(function ($) {
+    $(document).ready(function () {
+        $('.slider').slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 2
+        });
+    });
+});
