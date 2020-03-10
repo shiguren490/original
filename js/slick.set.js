b7bda9978739f309ce6ee40d760d3200e9d6721c
shiguren490 <60349708+shiguren490@users.noreply.github.com>
2020-03-10 23:01:21 +0900

    Update slick.set.js

--- js/slick.set.js
+++ js/slick.set.js
@@ -5,8 +5,27 @@ jQuery(function ($) {
             infinite: true,
             rows: 2,
             slidesPerRow: 3,
-            slidesToShow: 3,
-            slidesToScroll: 2,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+		},
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+		},
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+		}]
         });
     });
 });
